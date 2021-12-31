fe061f88b43b58f4c3ff4a46932118408ac95af5
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-31 23:01:16 +0500

    MFY auto commit at 31/12/2021 22:55:41

--- 2) Software Project Management_BM/Assignment/A2/FactoryMethods/src/com/company/ColoredShapes.java
+++ 2) Software Project Management_BM/Assignment/A2/FactoryMethods/src/com/company/ColoredShapes.java
@@ -1,4 +1,17 @@
 package com.company;
 
-public class ColoredShapes {
+import com.company.Interface.IShapesColors;
+
+public class ColoredShapes implements IShapesColors {
+
+    @Override
+    public void Colored(String nameOfShape) {
+        System.out.println("Give Colored to the shape of "+nameOfShape);
+    }
+
+    @Override
+    public void Border(String nameOfShape) {
+        // do nothings
+    }
 }
+
