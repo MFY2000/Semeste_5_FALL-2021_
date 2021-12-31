93563c01b6267d0f0308dd35eeb63cab4b836df4
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-31 15:17:26 +0500

    MFY auto commit at 31/12/2021 14:55:36

--- 2) Software Project Management_BM/Assignment/A2/FactoryMethods/src/com/company/FactoryShapes.java
+++ 2) Software Project Management_BM/Assignment/A2/FactoryMethods/src/com/company/FactoryShapes.java
@@ -3,39 +3,21 @@ package com.company;
 public class FactoryShapes {
     static IShapes[] NumberOfShapes = {new Circle(), new Triangle(), new Square()};
 
-    IShapes getShapes(String ShapeType){
-        for (var index: NumberOfShapes) {
-            System.out.println(index.nameOfShape);
-            if(index.nameOfShape == ShapeType){
-                return index;
-            }
-        }
-        return NumberOfShapes[0];
-    }
-
 
 }
-class Circle extends IShapes{
-    public void setNameOfShape(String nameOfShape) {
-        this.nameOfShape = "Circle";
-    }
-
+class Circle implements IShapes{
     @Override
     public void draw() {
         System.out.println("Circle is Draw");
     }
 }
-class Square extends IShapes{
-    public String nameOfShape = "Square";
-
+class Square implements IShapes{
     @Override
     public void draw() {
         System.out.println("Square is Draw");
     }
 }
-class Triangle extends IShapes{
-    public String nameOfShape = "Triangle";
-
+class Triangle implements IShapes{
     @Override
     public void draw() {
         System.out.println("Triangle is Draw");
