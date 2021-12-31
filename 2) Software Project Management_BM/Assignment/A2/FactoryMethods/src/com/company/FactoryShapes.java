76c04410344d9057f2f254e9ee9fe05428d0f47b
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-31 23:01:17 +0500

    MFY auto commit at 31/12/2021 22:55:41

--- 2) Software Project Management_BM/Assignment/A2/FactoryMethods/src/com/company/FactoryShapes.java
+++ 2) Software Project Management_BM/Assignment/A2/FactoryMethods/src/com/company/FactoryShapes.java
@@ -1,10 +1,35 @@
 package com.company;
 
+import com.company.Interface.IShapes;
+
 public class FactoryShapes {
-    static IShapes[] NumberOfShapes = {new Circle(), new Triangle(), new Square()};
 
+    public IShapes getShapes(String name){
+     IShapes NumberOfShapes;
+        if (name == "Circle"){
+            NumberOfShapes = new Circle();
+        }else if (name == "Triangle") {
+            NumberOfShapes = new Triangle();
+        }else if (name == "Square") {
+            NumberOfShapes = new Square();
+        }
+        else {
+            return null;
+        }
+        return NumberOfShapes;
+    }
 
+    public void setShapeStyle(String style, String Shape) {
+        if (style == "Border") {
+            (new BorderShapes()).Border(Shape);;
+        } else if (style == "Colored") {
+            (new ColoredShapes()).Colored(Shape);
+        } else {
+
+        }
+    }
 }
+
 class Circle implements IShapes{
     @Override
     public void draw() {
@@ -13,9 +38,7 @@ class Circle implements IShapes{
 }
 class Square implements IShapes{
     @Override
-    public void draw() {
-        System.out.println("Square is Draw");
-    }
+    public void draw() { System.out.println("Square is Draw"); }
 }
 class Triangle implements IShapes{
     @Override
