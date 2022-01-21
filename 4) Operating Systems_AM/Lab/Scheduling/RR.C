1af9262fe09633c0c8651a5c8955379f735e4f10
MFY2000 <fa19bsse0014@maju.edu.pk>
2022-01-21 16:03:43 +0500

    MFY auto commit at 21/01/2022 16:03:32

--- 4) Operating Systems_AM/Lab/Scheduling/RR.C
+++ 4) Operating Systems_AM/Lab/Scheduling/RR.C
@@ -63,3 +63,25 @@ int RR(){
     printf("\nAverage Waiting Time: \t%f", avg_tat);  
     getch();  
 }  
+
+
+
+#include<stdio.h
+#include<unistd.h>
+#include<sys/types.h>
+int main()
+{
+
+pid_t p;
+p=fork();
+if(p==0) //child
+{
+printf("I am child having PID %d\n",getpid());
+printf("My parent PID is %d\n",getppid());
+}
+else //parent
+{
+printf("I am parent having PID %d\n",getpid());
+printf("My child PID is %d\n",p);
+}
+}
